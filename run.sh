41a011c7b24077631ed4aa68eb39c51f0fbf04f0
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-03-13 16:36:38 -0700

    Clean up

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Launcher for bourne again shell
+#     * purges all containers and
+#       images and restarts
+docker stop $(docker ps -a -q)
+docker container prune -f
+docker image prune -af
+docker-compose -f docker-compose.yml up
