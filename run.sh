f3dad246af712a34e3c88b8d89d52757201ee920
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-07-31 21:29:12 -0700

    Clean and optimize conf

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Launcher for bourne again shell
-#     * purges all containers and
-#       images and restarts
-# docker stop $(docker ps -a -q)
-# docker container prune -f
-# docker image prune -af
-docker-compose -f docker-compose.yml up --build
